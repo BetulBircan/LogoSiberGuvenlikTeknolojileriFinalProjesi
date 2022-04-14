ef0ba058b838f4a854b320ae6189bbb76e751a5a
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-04-14 15:45:58 +0300

    routera açıklama eklendi

--- server/router/Router.js
+++ server/router/Router.js
@@ -1,9 +1,9 @@
-const express = require('express')
+const express = require('express') //express modülünü çağırma
 const Books =require('../controller/books')    //Kitaplar
 const Categories = require('../controller/categories') //Kategoriler
-const Authors = require('../controller/authors')
-const NewBooks = require('../controller/newbooks')
-const router = express.Router()
+const Authors = require('../controller/authors') //yazarlar
+const NewBooks = require('../controller/newbooks') //yenikitaplar
+const router = express.Router() //router nesnesini tanımlama
 
 //Books
 router.get('/books', Books.getBooks)
@@ -16,10 +16,12 @@ router.get('/getelastic', Books.getBookElastic)
 router.get('/categories', Categories.getCategories)
 router.get('/category', Categories.getCategory)
 
+//Authors
 router.get('/authors', Authors.getAuthors)
 router.get('/author', Authors.getAuthor)
 router.get('/authorscategory', Authors.getAuthorsFromCategories)
 
+//New Books
 router.get('/newbooks', NewBooks.getNewBooks)
 router.get('/newbook', NewBooks.getNewBook)
 
